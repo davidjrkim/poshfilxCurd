a9cb3e3ff687b3cd41a0be0692b1f74b7a62046b
David Kim <david@sharemind.kr>
2021-07-15 13:38:17 +0900

    init

---
+++ src/main/java/kr/sharemind/poshfilxCrud/PoshfilxCrudApplication.java
@@ -0,0 +1,13 @@
+package kr.sharemind.poshfilxCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoshfilxCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoshfilxCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/sharemind/poshfilxCrud/PoshfilxCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.sharemind.poshfilxCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoshfilxCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
